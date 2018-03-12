af38a3ac65852bce59cdd5d64de29433eac86a37
amylee <18981613235@163.com>
2018-03-12 08:33:08 +0800

    .

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+
+
+
+window.onscroll = function(){
+    console.log(window.scrollY);
+    if(window.scrollY > 0){
+        topNavBar.classList.add('sticky')
+    }else{
+        topNavBar.classList.remove('sticky');
+    }
+}
+
+var aTags = document.getElementsByClassName('menuTrigger');
+for(var let = 0; i < aTags; i++){
+    aTags[i].onmouseenter = function(){
+        aTags.addClassList = 
+    }
+    aTags
+}
